102c853823601649aabcf78fa2f8a2d2bb3d2079
ArtemErmulin <artem.ermulin@ya.ru>
2020-06-25 18:44:11 +0300

    Bug fix

--- davai_s_nami_bot/bot_engine.py
+++ davai_s_nami_bot/bot_engine.py
@@ -9,6 +9,8 @@ bot = TeleBot(BOT_TOKEN)
 
 @bot.message_handler(commands=["start"])
 def start_message(incoming_msg):
+    uid = incoming_msg.from_user.id
+    text = incoming_msg.text
     bot.send_message(chat_id=uid, text=incoming_msg)
 
 
