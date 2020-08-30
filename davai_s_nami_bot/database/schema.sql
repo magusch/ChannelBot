f96aa158374df285e27a64b51bb7d60b31b41589
magusch <magusch@gmail.com>
2020-08-30 16:20:05 +0300

    add trigger to scheme.sql

--- davai_s_nami_bot/database/schema.sql
+++ davai_s_nami_bot/database/schema.sql
@@ -5,3 +5,18 @@ CREATE TABLE IF NOT EXISTS "dev_events" (
     "event_date" TIMESTAMP,
     "date_to" TIMESTAMP
 );
+
+
+CREATE OR REPLACE FUNCTION date_from_to_date_to()
+RETURNS TRIGGER AS
+$$
+BEGIN 
+	UPDATE dev_events SET date_to=dev_events.date_from+ '2 hours' WHERE date_to is NULL;
+	RETURN NEW;
+END;
+$$
+LANGUAGE 'plpgsql';
+
+
+CREATE TRIGGER empty_date_to AFTER INSERT ON dev_events 
+FOR EACH ROW EXECUTE PROCEDURE date_from_to_date_to();
