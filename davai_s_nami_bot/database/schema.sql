4093d64e25093c70df983fcd56f8fa21eb679d8d
ArtemErmulin <artem.ermulin@ya.ru>
2020-09-23 17:58:10 +0300

    Rename database table for test

--- davai_s_nami_bot/database/schema.sql
+++ davai_s_nami_bot/database/schema.sql
@@ -1,4 +1,4 @@
-CREATE TABLE IF NOT EXISTS "dev_events" (
+CREATE TABLE IF NOT EXISTS "test_events" (
     "id" TEXT UNIQUE,
     "title" TEXT NOT NULL,
     "post_id" INTEGER,
@@ -11,12 +11,12 @@ CREATE OR REPLACE FUNCTION date_from_to_date_to()
 RETURNS TRIGGER AS
 $$
 BEGIN
-    UPDATE dev_events SET date_to=dev_events.date_from+ '2 hours' WHERE date_to is NULL;
+    UPDATE test_events SET date_to=test_events.date_from+ '2 hours' WHERE date_to is NULL;
     RETURN NEW;
 END;
 $$
 LANGUAGE 'plpgsql';
 
 
-CREATE TRIGGER empty_date_to AFTER INSERT ON dev_events
+CREATE TRIGGER empty_date_to AFTER INSERT ON test_events
 FOR EACH ROW EXECUTE PROCEDURE date_from_to_date_to();
