73c2fc7bccc433211a04c02a37f4dfd86cd26617
ArtemErmulin <artem.ermulin@ya.ru>
2021-01-24 20:09:31 +0300

    Fix typo

--- dump_secrets.py
+++ dump_secrets.py
@@ -3,5 +3,5 @@ import os
 from davai_s_nami_bot import utils
 
 with open("prod_constants", "w") as file:
-    for name in utils:
+    for name in utils.REQUIRED_CONSTANT_NAMES:
         file.write(f"{name} {os.environ.get(name)}")
