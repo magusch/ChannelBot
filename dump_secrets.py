7ce04ee6014d73cc5ec6506c4ad664740a60979a
ArtemErmulin <artem.ermulin@ya.ru>
2021-01-24 20:10:46 +0300

    Fix typo

--- dump_secrets.py
+++ dump_secrets.py
@@ -4,4 +4,4 @@ from davai_s_nami_bot import utils
 
 with open("prod_constants", "w") as file:
     for name in utils.REQUIRED_CONSTANT_NAMES:
-        file.write(f"{name} {os.environ.get(name)}")
+        file.write(f"{name} {os.environ.get(name)}\n")
