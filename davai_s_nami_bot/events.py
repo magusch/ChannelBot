e474487fd9d2b63587bb254a95b5d82d215dee7d
magusch <magusch@gmail.com>
2021-11-06 14:18:13 +0300

    add approved organizations

--- davai_s_nami_bot/events.py
+++ davai_s_nami_bot/events.py
@@ -20,15 +20,20 @@ BAD_KEYWORDS = (
     "консультация",
 )
 APPROVED_ORGANIZATIONS = [
-    "57992",  # Манеж
-    "79462",  # Новая Голландия
-    "42587",  # Молодёжный центр Эрмитажа
+    "57992",   # Манеж
+    "79462",   # Новая Голландия
+    "42587",   # Молодёжный центр Эрмитажа
     "186669",  # musicAeterna
     "109981",  # ГЦСИ в Санкт-Петербурге
-    "67092",  # Музей советских игровых автоматов
-    "43027",  # Театр-студия
-    "78132",  # Театр-фестиваль «Балтийский дом»
-    "75134",  # Ленфильм
+    "67092",   # Музей советских игровых автоматов
+    "43027",   # Театр-студия
+    "78132",   # Театр-фестиваль «Балтийский дом»
+    "75134",   # Ленфильм
+    "191811",  # Планетарий 1
+    "130063",  # МИСП
+    "267817",  # Маяковка
+    "30148",   # ЦСИ Курёхина
+    "112209",  # Севкабель
 ]
 
 BORING_ORGANIZATIONS = [
@@ -37,7 +42,7 @@ BORING_ORGANIZATIONS = [
     "212547",  # Иерусалимская Сказка
     "146675",  # Корни и Крылья https://korni-i-krylya.timepad.ru
     "252995",  # Музей Христианской Культуры
-    "63354",  # Семейный досуговый клуб ШтангенЦиркулб
+    "63354",   # Семейный досуговый клуб ШтангенЦиркулб
     "181043",  # Фонд
 ]
 
@@ -55,7 +60,7 @@ CATEGORY_IDS_EXCLUDE = [
     "461",  # Экскурсии и путешествия
     "462",  # Другие события
 ]
-STARTS_AT_MIN = "{year_month_day}T11:00:00"
+STARTS_AT_MIN = "{year_month_day}T10:00:00"
 STARTS_AT_MAX = "{year_month_day}T23:59:00"
 TIMEPAD_APPROVED_PARAMS = dict(
     limit=100,
@@ -70,7 +75,7 @@ TIMEPAD_OTHERS_PARAMS = dict(
     organization_ids_exclude=(
         ", ".join(APPROVED_ORGANIZATIONS) + ", " + ", ".join(BORING_ORGANIZATIONS)
     ),
-    price_max=1000,
+    price_max=1200,
     category_ids_exclude=", ".join(CATEGORY_IDS_EXCLUDE),
     keywords_exclude=", ".join(BAD_KEYWORDS),
 )
@@ -82,7 +87,7 @@ TICKETSCLOUD_ORG_IDS = ['5dce558174fd6b0bcaa66524','5e3d551b44d20ecf697408e4', '
                         '5fd84f24ae1e29b732c6756c','5dd47966c189df3040c1ae3a', '5bb25b9ee5b64d000cfcc38c', '5c01321a269b85000becd652',
                         '5c7950a93df5de000c93e287', '5f75c8e17540a6f988fa0a1f', '6036cfb79ad7272eea7734bf', '5f104439d473aea92c126338',
                         '5daf03692c4c8cd18ef6b0da','5bb3766290566f000b409adf','5d8cd897cb535bfd631b7348', '5f5a3a5e50f7d892d28ffdb5',
-                        '5f6d96bf06a12bb6586080c4', '5c6eac03afa1a9000cc77e34' ]
+                        '5f6d96bf06a12bb6586080c4', '5c6eac03afa1a9000cc77e34', '5e060d4c36db15fb7f777ae8' ]
 
 
 ## PARSERS
